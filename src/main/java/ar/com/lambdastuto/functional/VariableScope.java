1b8a2adc8d1df7dd713d1af8de4370e15439e5d2
marelas <marela.as@gmail.com>
2024-02-06 22:54:06 -0300

    adding variable scope

---
+++ src/main/java/ar/com/lambdastuto/functional/VariableScope.java
@@ -0,0 +1,18 @@
+package ar.com.lambdastuto.functional;
+
+import java.util.List;
+import java.util.function.IntConsumer;
+
+public class VariableScope {
+    static int k = 0;
+    public static void main(String[] args) {
+        int b = 10; // local variable
+
+        IntConsumer intConsumer = (a) -> System.out.println(a*10);
+
+        List<Instructor> instructorList = Instructors.getAll();
+        instructorList.forEach(instructor -> {
+                    System.out.println(instructor + " " + k);
+                });
+    }
+}
